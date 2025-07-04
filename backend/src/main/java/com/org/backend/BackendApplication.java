0395052d8bae054ce1d545b6e2edf61e783d5b99
Albert-Toth Szilard <albert.szilard@sonrisa.hu>
2025-07-04 12:06:43 +0200

    Init backend

--- backend/src/main/java/com/org/backend/BackendApplication.java
+++ backend/src/main/java/com/org/backend/BackendApplication.java
@@ -1,13 +1,17 @@
 package com.org.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		ApplicationContext context = SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
